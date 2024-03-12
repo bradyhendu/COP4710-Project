9a9fcc1bb853a2f7b58152c36371d6f9e433b38f
Brady Henderson <bradyhendu@gmail.com>
2024-03-12 13:53:51 -0400

    adds colors for a theme

--- nextjs-flask/tailwind.config.js
+++ nextjs-flask/tailwind.config.js
@@ -11,7 +11,11 @@ module.exports = {
         'hero-pattern': "url('../images/movies.jpg')",
       },
       colors: {
-        'nav-blue': '#1976D2',
+        'primary': '#1976D2',
+        'secondary': '#d6919e',
+        'text': '#091118',
+        'accent': '#c8a26a',
+        'dark-text': '#e9f0f7',
       }
       },
   },
