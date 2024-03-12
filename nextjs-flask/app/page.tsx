154ccc10f50674d235df3c701a081a45a5e84da5
Brady Henderson <bradyhendu@gmail.com>
2024-03-12 14:16:45 -0400

    gets rid of h-screen

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 const MyPage = () => {
 
   return (
-    <div className='bg-hero-pattern flex flex-col items-center justify-center w-screen h-screen text-center'>
+    <div className='bg-hero-pattern flex flex-col items-center justify-center h-screen text-center'>
       <div className='bg-primary p-6 rounded-xl'>
-        <h1 className="text-5xl font-bold">Welcome to CineFan</h1>
-        <p>The place for all your movie needs</p>
+        <h1 className="text-5xl font-bold">Welcome to <span className='text-secondary'>CineFan</span></h1>
+        <p className='mt-6'>The place for all your movie needs</p>
+        <Link href='/register'>
+          <button className='mt-6 bg-secondary hover:bg-accent text-white px-4 py-2 rounded-lg'>Get Started</button>
+        </Link>
       </div>
     </div>
   );
