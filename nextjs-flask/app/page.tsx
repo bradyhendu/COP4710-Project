4b19461566a2d212031282faf528c08e144e933f
Brady Henderson <bradyhendu@gmail.com>
2024-03-12 13:58:56 -0400

    matches the new color name

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -4,7 +4,7 @@ const MyPage = () => {
 
   return (
     <div className='bg-hero-pattern flex flex-col items-center justify-center w-screen h-screen text-center'>
-      <div className='bg-nav-blue p-6 rounded-xl'>
+      <div className='bg-primary p-6 rounded-xl'>
         <h1 className="text-5xl font-bold">Welcome to CineFan</h1>
         <p>The place for all your movie needs</p>
       </div>
