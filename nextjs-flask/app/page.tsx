1e2d2d05ec7e45c6391f8b3457d7f250b44bce93
Brady Henderson <bradyhendu@gmail.com>
2024-03-12 13:49:11 -0400

    Makes a simple home page

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -1,11 +1,13 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const MyPage = () => {
 
   return (
-    <div>
-      
+    <div className='bg-hero-pattern flex flex-col items-center justify-center w-screen h-screen text-center'>
+      <div className='bg-nav-blue p-6 rounded-xl'>
+        <h1 className="text-5xl font-bold">Welcome to CineFan</h1>
+        <p>The place for all your movie needs</p>
+      </div>
     </div>
   );
 };
