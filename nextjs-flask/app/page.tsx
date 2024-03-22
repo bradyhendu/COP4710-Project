51d54cc4a610e302ce55115e82e75854ecd6f6aa
Brady Henderson <bradyhendu@gmail.com>
2024-03-22 15:34:03 -0400

    gives more of a padding to the center div

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 const MyPage = () => {
 
   return (
-    <div className='bg-hero-pattern flex flex-col items-center justify-center h-screen text-center'>
-      <div className='bg-primary p-6 rounded-xl'>
+    <div className='bg-hero-pattern  flex flex-col items-center justify-center h-screen text-center'>
+      <div className='bg-primary p-12 rounded-xl'>
         <h1 className="text-5xl font-bold">Welcome to <span className='text-secondary'>CineFan</span></h1>
         <p className='mt-6'>The place for all your movie needs</p>
         <Link href='/register'>
