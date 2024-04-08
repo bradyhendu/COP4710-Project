88502f311311af447d5e60831835bdfcc3f502e8
Brady Henderson <bradyhendu@gmail.com>
2024-04-08 15:46:04 -0400

    change to mock epantry

--- nextjs-flask/database/server.py
+++ nextjs-flask/database/server.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True, origins=['http://localhost:3000'])
+
+app.secret_key = "i-am_secret,_sooooooo_so-secret"
 
 @app.route("/")
 def hello_world():
