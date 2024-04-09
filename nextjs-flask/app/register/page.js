0e721d056b3a444b08094449e88698d19a18b491
Brady Henderson <bradyhendu@gmail.com>
2024-04-08 22:45:19 -0400

    Makes a working register button

--- nextjs-flask/app/register/page.js
+++ nextjs-flask/app/register/page.js
@@ -18,6 +18,15 @@ const Register = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log(formData)
+
+    const response = fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(formData)
+    })
   }
 
   return (
@@ -26,10 +35,14 @@ const Register = () => {
         <h1 className="text-5xl font-bold">Create an account</h1>
         <p className='mt-2'>Already have an account? <Link href='/login' className='text-secondary hover:text-accent'>Login</Link></p>
         <form className='mt-6 flex flex-col items-center' onSubmit={handleSubmit}>
-          <input type='text' placeholder='Username' className='p-2 rounded-lg text-dark' value={formData.username} onChange={handleChange}/>
-          <input type='email' placeholder='Email' className='p-2 mt-2 rounded-lg text-dark' value={formData.email} onChange={handleChange}/>
-          <input type='password' placeholder='Password' className='p-2 mt-2 rounded-lg text-dark' value={formData.password} onChange={handleChange}/>
-          <input type='password' placeholder='Confirm Password' className='p-2 mt-2 rounded-lg text-dark' value={formData.confirmPassword} onChange={handleChange}/>
+          <label htmlFor='username'>Username</label>
+          <input type='text' placeholder='Username' className='p-2 rounded-lg text-dark' name='username' value={formData.username} onChange={handleChange}/>
+          <label htmlFor='email' className='mt-2'>Email</label>
+          <input type='email' placeholder='Email' className='p-2  rounded-lg text-dark' name='email' value={formData.email} onChange={handleChange}/>
+          <label htmlFor='password' className='mt-2'>Password</label>
+          <input type='password' placeholder='Password' className='p-2 rounded-lg text-dark' name='password' value={formData.password} onChange={handleChange}/>
+          <label htmlFor='confirmPassword' className='mt-2'>Confirm Password</label>
+          <input type='password' placeholder='Confirm Password' className='p-2 rounded-lg text-dark' name='confirmPassword' value={formData.confirmPassword} onChange={handleChange}/>
           <button type='submit' className='mt-6 bg-secondary hover:bg-accent text-white px-4 py-2 rounded-lg'>Register</button>
         </form>
       </div>
