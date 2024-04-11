b0b66488df69ac5d42b1debb5ec20f02be6d0f2e
Ryan Ly <ryanly@Ryans-Air.domain>
2024-04-11 18:00:59 -0400

    Fully Functional Register and Login

--- nextjs-flask/database/server.py
+++ nextjs-flask/database/server.py
@@ -1,21 +1,24 @@
 from flask import Flask, jsonify, session, request
 from flask_cors import CORS
 import psycopg2
+import hashlib
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True, origins=['http://localhost:3000'])
 
 app.secret_key = "i-am_secret,_sooooooo_so-secret"
 
+# Get Connection to the DB w/ superuser
 def get_superuser_conn():
     connection = psycopg2.connect(
         host="localhost",
             database="moviesearch", #mine is lowercase, update to match yours
             user="postgres",
-            password="Harley69?!" #edit to match your password
+            password="dbisFun@24" #edit to match your password
     )
     return connection
 
+# Function to Keep User Logged In If Never Logged Out
 @app.route('/check_session', methods=['GET'])
 def check_session(): 
     if 'username' in session:
@@ -25,11 +28,13 @@ def check_session():
         print("No Session")
         return jsonify({'username': None}), 200
     
+# Function to Log a User Out
 @app.route('/logout', methods=['POST'])
 def logout():
     session.pop('username', None)
     return jsonify({'message': 'success'}), 200
 
+# Function to Register Users
 @app.route('/register', methods=['POST'])
 def register_user():
     print("On Register Page")
@@ -54,6 +59,10 @@ def register_user():
             cur.execute("GRANT movie_user TO \"" + str(data['username']) + "\"")
             print("Executed GRANT Permissions Query")
             conn.commit()
+            cur.execute("INSERT INTO Movie_User(userID, f_name, l_name, email) \
+                        VALUES('" + str(data['username']) + "', '" + str(data['firstName']) + "', '" \
+                        + str(data['lastName']) + "', '" + str(data['email']) + "')")
+            conn.commit()
             session['username'] = data['username']
             print("Created User")
             return jsonify({'message': 'User created successfully', 'username': data['username']}), 201
@@ -64,6 +73,39 @@ def register_user():
         cur.close()
         conn.close()
 
+# Function for Logging In
+@app.route('/login', methods=['POST'])
+def login_user():
+    print("On Login Page")
+    conn = get_superuser_conn()
+    cur = conn.cursor()
+    # Extract form data from the request
+    data = request.json
+    try:
+        # Check if the username exists
+        cur.execute("SELECT rolname, rolpassword FROM pg_authid WHERE rolname = '" + str(data['username']) + "'")
+        existing_user = cur.fetchone()
+        print(data)
+        if existing_user:
+            print("User Exists, Check Password")
+            # Encrypt the Password, Concatenated w/ 'md5'
+            hashed_pw = str(data['password']) + str(data['username'])
+            hashed_pw = hashlib.md5(hashed_pw.encode())
+            hashed_pw = hashed_pw.hexdigest()
+            hashed_pw = 'md5' + hashed_pw
+            # Check if the password is correct
+            if existing_user[1] == hashed_pw:
+                session['username'] = data['username']
+                return {'message': 'Login successful', 'username': data['username']}, 200
+            else:
+                return {'message': 'Incorrect password'}, 401  # Unauthorized
+        else:
+            return {'message': 'User not found'}, 404  # Not Found
+    except Exception as e:
+        return {'message': 'Internal Server Error', 'error': str(e)}, 500  # Internal Server Error
+    finally:
+        cur.close()
+        conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
