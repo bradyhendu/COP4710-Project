09454658d85e1cc6dc94c125dfc8d287088d834e
Brady Henderson <bradyhendu@gmail.com>
2024-04-19 23:37:44 -0400

    actors and movie pages

--- nextjs-flask/app/actors/page.js
+++ nextjs-flask/app/actors/page.js
@@ -1,9 +1,54 @@
-import React from 'react'
-
+'use client';
+import React, {useEffect, useState, useCallback} from 'react'
+import Link from 'next/link'
 const Actors = () => {
+  const [actors, setActors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(''); // State to hold the search term
+
+  //Pulls all actors from the database
+  const fetchActors = useCallback(async () => {
+    const response = await fetch('http://localhost:8080/getactors', {
+      credentials: 'include',
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setActors(data);
+      console.log(data);
+    } else {
+      console.error('Failed to fetch actors');
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchActors();
+    console.log(actors);
+  }, [fetchActors]);
+
+  //handle search term
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredActors = actors.filter((actor) => {
+    return actor.actor_name.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+  );
+
   return (
-    <div>
-      <h1>Actors</h1>
+    <div className="flex items-center flex-col justify-center mt-20">
+      <h1 className='text-5xl font-bold text-white'>Have a favorite actor?</h1>
+      <div>
+        <input type="text" placeholder="Search for an actor" className="border-2 border-black rounded-lg p-2 m-2 text-black" onChange={handleSearchChange}/>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {filteredActors.map((actor, index) => (
+          <div key={index} className="m-2 p-2 border-2 border-black rounded-lg bg-primary">
+            <Link href={`/actors/${actor.actor_id}`}> 
+                <h2 className="text-2xl font-bold text-white">{actor.actor_name}</h2>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- nextjs-flask/app/movies/page.js
+++ nextjs-flask/app/movies/page.js
@@ -1,10 +1,59 @@
-import React from 'react'
+'use client';
+import React, {useEffect, useState, useCallback} from 'react'
+import Link from 'next/link'
+
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(''); // State to hold the search term
+
+  //Pulls all movies from the database
+  const fetchMovies = useCallback(async () => {
+    const response = await fetch('http://localhost:8080/getmovies', {
+      credentials: 'include',
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setMovies(data);
+      console.log(data);
+    } else {
+      console.error('Failed to fetch movies');
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
+  
+  //handle search term
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) => {
+    return movie.movie_title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+  );
+   
+
   return (
-    <div>
-      <h1>Movies</h1>
+    <div className="flex items-center flex-col justify-center mt-20">
+      <h1 className='text-5xl font-bold text-white'>Wanna Watch a Movie?</h1>
+      <div>
+        <input type="text" placeholder="Search for a movie" className="border-2 border-black rounded-lg p-2 m-2 text-black" onChange={handleSearchChange}/>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {filteredMovies.map((movie, index) => (
+          <div key={index} className="m-2 p-2 border-2 border-black rounded-lg bg-primary">
+            <Link href={`/movies/${movie.movie_id}`}> 
+                <h2 className="text-2xl font-bold text-white">{movie.movie_title}</h2>
+                <p className="text-white">{movie.movie_id}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
+
   )
 }
 
