33d1ba098f2ffee1d3e4fc955faed3537ccc168a
Ryan Ly <ryanly@Ryans-Air.domain>
2024-04-21 12:09:15 -0400

    Added Functionality for [actorID] page

--- nextjs-flask/database/server.py
+++ nextjs-flask/database/server.py
@@ -111,7 +111,7 @@ def login_user():
 def get_actors():
     conn = get_superuser_conn()
     cur = conn.cursor()
-    cur.execute("SELECT fname, lname, Actor.actorID \
+    cur.execute("SELECT DISTINCT fname, lname, Actor.actorID \
                     FROM Actor \
                     INNER JOIN Acts ON Actor.actorID = Acts.actorID")
     actors = cur.fetchall()
@@ -135,8 +135,46 @@ def get_actors_of_movie():
     conn.close()
     return jsonify([{"actor_name": actor[0] + " " + actor[1], "actor_id": actor[2]} for actor in actors])
 
+@app.route('/getmoviesofactor', methods=['POST'])
+def get_movies_of_actor():
+    print("Getting Movies the Actor Has Starred In")
+    conn = get_superuser_conn()
+    cur = conn.cursor()
+    data = request.json
+    print(data)
+    cur.execute("SELECT title, Movie.movieID \
+                FROM Movie \
+                INNER JOIN Acts ON Movie.movieID = Acts.movieID \
+                INNER JOIN Actor ON Acts.actorID = Actor.actorID \
+                WHERE Actor.actorID = %s", (data['actor_id'],))
+    movies = cur.fetchall()
+    print("movies: ", movies)
+    cur.close()
+    conn.close()
+    return jsonify([{'movie_title': movie[0], 'movie_id': movie[1]} for movie in movies])
+
+@app.route('/actors/<string:actor_id>', methods=['GET'])
+def get_actor_details(actor_id):
+    conn = get_superuser_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT fname, lname, dob, since \
+                FROM Actor \
+                WHERE actorID = %s", (actor_id,))
+    actor = cur.fetchone()
+    cur.close()
+    conn.close()
+    #convert time to string
+    if actor is None:
+        # No actor found, return an appropriate response
+        return jsonify({"error": "No Actor Found With the Provided ID"}), 404
+
+    actor = list(actor)
+    
+    return jsonify({'name': actor[0] + " " + actor[1], 'date_of_birth': actor[2], 'since': actor[3]})
+
 @app.route('/movies/<string:movie_id>', methods=['GET'])
 def get_movie_details(movie_id):
+    print(movie_id)
     conn = get_superuser_conn()
     cur = conn.cursor()
     cur.execute("SELECT title, release_date, duration \
@@ -159,14 +197,14 @@ def get_movie_details(movie_id):
 def get_movies():
     conn = get_superuser_conn()
     cur = conn.cursor()
-    cur.execute("SELECT title, string_agg(genre, ', ') \
+    cur.execute("SELECT title, Movie.movieID, string_agg(genre, ', ') \
                 FROM Movie \
                 INNER JOIN Movie_Genre ON Movie.movieID = Movie_Genre.movieID \
-                GROUP BY Movie.title")
+                GROUP BY Movie.title, Movie.movieID")
     movies = cur.fetchall()
     cur.close()
     conn.close()
-    return jsonify([{'movie_title': movie[0], 'movie_id': movie[1]} for movie in movies])
+    return jsonify([{'movie_title': movie[0], 'movie_id': movie[1], 'genres': movie[2]} for movie in movies])
 
 @app.route('/getmoviegenre', methods=['GET'])
 def get_movie_genre():
@@ -174,21 +212,6 @@ def get_movie_genre():
     cur = conn.cursor()
     data = request.json
     cur.execute("SELECT title FROM Movie WHERE genre = %s", (data['genre_type'],))
-    movies = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify({'movie_title': movie[0]} for movie in movies)
-
-@app.route('/getmoviesofactor', methods=['GET'])
-def get_movies_of_actor():
-    conn = get_superuser_conn()
-    cur = conn.cursor()
-    data = request.json
-    cur.execute("SELECT title \
-                FROM Movie \
-                INNER JOIN Acts ON Movie.movieID = Acts.movieID \
-                INNER JOIN Actor ON Acts.actorID = Actor.actorID \
-                WHERE Actor.actorID = %s", (data['actor'],))
     movies = cur.fetchall()
     cur.close()
     conn.close()
@@ -252,6 +275,23 @@ def recommendation_list():
         cur.close()
         conn.close()
         return jsonify({'message': 'No Recommendations At the Moment'})
+    
+@app.route('/addreview', methods=['POST'])
+def add_review():
+    conn = get_superuser_conn()
+    cur = conn.cursor()
+    data = request.json
+    try:
+        cur.execute("SELECT movieID FROM Movie WHERE movieID = %S", (data['movie_ID'],))
+        movie = cur.fetchone()
+        cur.execute("INSERT INTO Review(rating, review_content, username, movieID) VALUES \
+                    (%s, %s, %s, %s)", (data['rate'], data['content'], data['user_name'], movie,))
+        return jsonify({'message': 'Review Added Successfully'})
+    except Exception as e:
+        return {'message': 'Internal Server Error', 'error': str(e)}, 500  # Internal Server Error
+    finally:
+        cur.close()
+        conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
