6c6a3d2d963bfd94d66b4566253a7866068718c2
Brady Henderson <bradyhendu@gmail.com>
2024-04-22 17:03:32 -0400

    small changes

--- nextjs-flask/app/login/page.js
+++ nextjs-flask/app/login/page.js
@@ -44,7 +44,7 @@ const Login = () => {
           <input type='text' placeholder='Username' className='p-2 rounded-lg text-dark' name='username' value={formData.username} onChange={handleChange}/>
           <label htmlFor='password' className='mt-2'>Password</label>
           <input type='password' placeholder='Password' className='p-2 rounded-lg text-dark' name='password' value={formData.password} onChange={handleChange} />
-          <button type='submit' className='mt-6 bg-secondary hover:bg-accent text-white px-4 py-2 rounded-lg'>Register</button>
+          <button type='submit' className='mt-6 bg-secondary hover:bg-accent text-white px-4 py-2 rounded-lg'>Login</button>
         </form>
       </div>
     </div>
