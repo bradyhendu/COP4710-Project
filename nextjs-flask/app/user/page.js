cfce1eb31c98f78134cf0751b011c5aef7f4bdad
Brady Henderson <bradyhendu@gmail.com>
2024-04-22 20:10:28 -0400

    updated user page

--- nextjs-flask/app/user/page.js
+++ nextjs-flask/app/user/page.js
@@ -33,15 +33,15 @@ const User = () => {
   }, [fetchReviewedMovies, fetchUserDetails])
 
   return (
-    <div className='flex flex-col justify-center items-center mt-10'>
+    <div className='flex flex-col justify-center items-center mt-10 text-center'>
       <h1 className='text-5xl font-bold text-white'>Welcome, {user.first_name}</h1>
       <h2 className='text-3xl font-bold text-white mt-10'>Here are the movies you've reviewed:</h2>
       {movies.length === 0 ? (
         <div className="text-white mt-5">
-          No reviews yet, go <Link href="/movies">here</Link> to find movies.
+          No reviews yet, go <Link href="/movies" className='text-primary hover:text-secondary'>here</Link> to find movies.
         </div>
       ) : (
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center text-left">
           {movies.map((movie, index) => (
             <div key={index} className="m-2 p-2 border-2 border-black rounded-lg bg-primary">
               <Link href={`/movies/${movie.movie_id}`}> 
