8f3d45f7472e58e9d4dbf5c03f546fac39e5e737
Brady Henderson <bradyhendu@gmail.com>
2024-04-22 20:07:23 -0400

    Changing the user page

--- nextjs-flask/app/user/page.js
+++ nextjs-flask/app/user/page.js
@@ -36,16 +36,23 @@ const User = () => {
     <div className='flex flex-col justify-center items-center mt-10'>
       <h1 className='text-5xl font-bold text-white'>Welcome, {user.first_name}</h1>
       <h2 className='text-3xl font-bold text-white mt-10'>Here are the movies you've reviewed:</h2>
-      <div className="flex flex-wrap justify-center">
-        {movies.map((movie, index) => (
-          <div key={index} className="m-2 p-2 border-2 border-black rounded-lg bg-primary">
-            <Link href={`/movies/${movie.movie_id}`}> 
-                <h2 className="text-2xl font-bold text-white">{movie.title}</h2>
-                <p className='text-white'>{movie.genres}</p>
-            </Link>
-          </div>
-        ))}
-      </div>
+      {movies.length === 0 ? (
+        <div className="text-white mt-5">
+          No reviews yet, go <Link href="/movies">here</Link> to find movies.
+        </div>
+      ) : (
+        <div className="flex flex-wrap justify-center">
+          {movies.map((movie, index) => (
+            <div key={index} className="m-2 p-2 border-2 border-black rounded-lg bg-primary">
+              <Link href={`/movies/${movie.movie_id}`}> 
+                  <h2 className="text-2xl font-bold text-white">{movie.title}</h2>
+                  <p className='text-white'>{movie.genres}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+      <h2 className='text-3xl font-bold text-white mt-10'>Looking for new movies? Here's our reccomendations:</h2>
     </div>
   )
 }
